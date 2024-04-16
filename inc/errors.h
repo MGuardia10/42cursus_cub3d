73aad4c65ee7460351f9b1b2515599e98ffdfdc6
Miguel Guardia <mike.guardia0309@gmail.com>
2024-04-16 10:18:33 +0200

    some more error management

--- inc/errors.h
+++ inc/errors.h
@@ -6,7 +6,7 @@
 /*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:11:05 by mguardia          #+#    #+#             */
-/*   Updated: 2024/04/15 09:50:17 by mguardia         ###   ########.fr       */
+/*   Updated: 2024/04/16 09:51:36 by mguardia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,12 @@
 # define MISSING_PATH	"path to texture is missing\n"
 # define MISSING_COLOR	"color in rgb format is missing\n"
 # define TOO_MUCH_PATH	"too much items found, use only one path to texture\n"
-# define TOO_MUCH_COLOR	"too much items found, use noly one rgb color\n"
+# define TOO_MUCH_COLOR	\
+				"too much items found, use format [r,g,b] without spaces\n"
+# define NEGATIVE_COLOR	"negative found, insert only values from 0 to 255\n"
+# define INV_CHAR_COLOR	"invalid character, insert only values from 0 to 255\n"
+# define MAX_NUM_COLOR	"invalid number, insert only values from 0 to 255\n"
+# define FLOOR_TWICE	"floor item appears more than once\n"
+# define CEILING_TWICE	"ceiling item appears more than once\n"
 
 #endif
